3c56fd7471cc4c6109b20ec6517c405bc144c113
JooYooGit <jimmy.zhuyu@gmail.com>
2019-02-06 07:40:41 +0100

    added bootstrap

--- scripts/camera.js
+++ scripts/camera.js
@@ -2,13 +2,13 @@ var amountOfCatImages = document.getElementById('amountOfCatImages');
 
 
 // get stream from cam
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-        console.log('video play!!!!')
-    });
-}
+// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//     navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+//         video.srcObject = stream;
+//         video.play();
+//         console.log('video play!!!!')
+//     });
+// }
 
 //take screenshot
 function takeScreenshot() {
