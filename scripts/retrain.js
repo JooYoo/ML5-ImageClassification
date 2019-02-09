cc7c01b73cfab868a75f73763ae3e2e28fb75ed8
JooYooGit <jimmy.zhuyu@gmail.com>
2019-02-09 09:04:47 +0100

    fixed video problem

--- scripts/retrain.js
+++ scripts/retrain.js
@@ -1,7 +1,5 @@
 // document.addEventListener("DOMContentLoaded", function () {
 
-
-
   var video = document.getElementById('video');
   var videoStatus = document.getElementById('videoStatus');
   var loading = document.getElementById('loading');
@@ -13,87 +11,87 @@
   var result = document.getElementById('result');
   var predict = document.getElementById('predict');
 
+
   // A variable to store the total loss
   let totalLoss = 0;
 
   // Create a webcam capture
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-      video.srcObject = stream;
-      video.play();
-    });
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+          video.srcObject = stream;
+          video.play();
+      });
   }
 
   // Extract the already learned features from MobileNet
   // 默认课以区分两个category
-
   const featureExtractor = ml5.featureExtractor('MobileNet', { numClasses: 3 }, function () {
-    // const featureExtractor = ml5.featureExtractor('MobileNet', function () {
-    loading.innerText = 'Model loaded!';
-  });
-
-  // Create a new classifier using those features
-  const classifier = featureExtractor.classification(video, function () {
-    videoStatus.innerText = 'Video ready!';
-  });
-
-
-  // BLUE_btn:  
-  // press to add current frame with a label of blue to the classifier
-  blueButton.onclick = function () {
-    classifier.addImage('blue');
-    console.log('add Blue')
-  }
-
-  // RED_btn:
-  // press to add current frame with a label of red to the classifier
-  redButton.onclick = function () {
-    classifier.addImage('red');
-    console.log('add Red')
-  }
-
-  // BLACK_btn:
-  // press to add current frame with a label of black to the classifier
-  blackButton.onclick = function () {
-    classifier.addImage('black');
-    console.log('add Black')
-  }
-
-
-  // TRAIN_btn: 
-  // train the classifier with all the given images
-  train.onclick = function () {
-    classifier.train(function (lossValue) {
-      if (lossValue) {
-        console.log(lossValue);
-        totalLoss = lossValue;
-        loss.innerHTML = 'Loss: ' + totalLoss;
-      } else {
-        loss.innerHTML = 'Done Training! Final Loss: ' + totalLoss;
+      // const featureExtractor = ml5.featureExtractor('MobileNet', function () {
+      loading.innerText = 'Model loaded!';
+      // // Create a new classifier using those features
+      const classifier = featureExtractor.classification(video, videoReady);
+      function videoReady() {
+          videoStatus.innerText = 'Video ready!';
       }
-    });
-  }
 
-  // Predict the current frame.
-  function predict() {
-    classifier.predict(gotResults);
-  }
 
-  // Start predicting when the predict button is clicked
-  predict.onclick = function () {
-    classifier.classify(gotResults);
-    console.log()
-  }
+      // BLUE_btn:  
+      // press to add current frame with a label of blue to the classifier
+      blueButton.onclick = function () {
+          classifier.addImage('blue');
+          console.log('add Blue')
+      }
 
-  // Show the results
-  function gotResults(err, data) {
-    // Display any error
-    if (err) {
-      console.error(err);
-    }
-    result.innerText = data;
+      // RED_btn:
+      // press to add current frame with a label of red to the classifier
+      redButton.onclick = function () {
+          classifier.addImage('red');
+          console.log('add Red')
+      }
 
-    classifier.classify(gotResults);
-  }
+      // BLACK_btn:
+      // press to add current frame with a label of black to the classifier
+      blackButton.onclick = function () {
+          classifier.addImage('black');
+          console.log('add Black')
+      }
 
-// });
+
+      // TRAIN_btn: 
+      // train the classifier with all the given images
+      train.onclick = function () {
+          classifier.train(function (lossValue) {
+              if (lossValue) {
+                  console.log(lossValue);
+                  totalLoss = lossValue;
+                  loss.innerHTML = 'Loss: ' + totalLoss;
+              } else {
+                  loss.innerHTML = 'Done Training! Final Loss: ' + totalLoss;
+              }
+          });
+      }
+
+
+      // Show the results
+      function gotResults(err, data) {
+          // Display any error
+          if (err) {
+              console.error(err);
+          }
+          result.innerText = data;
+
+          classifier.classify(gotResults);
+      }
+
+      // Start predicting when the predict button is clicked
+      predict.onclick = function () {
+          classifier.classify(gotResults);
+          console.log('result')
+      }
+
+
+
+
+  });
+
+
